372cd0968967e828a3020f5886f4e1e214172404
D2B <suporte@d2bonline.com.br>
2022-07-25 15:34:22 -0300

    select by countries

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,23 +4,33 @@ import { useStateContext } from '../../contexts/contextProvider';
 import './header.css';
 
 const Header = () => {
-  const { setInputValue } = useStateContext();
+  const { setInputValue, selectValues } = useStateContext();
 
   return (
     <header className='header'>
-      <a href='/' className='header__title'>City Bike</a>
-      <select
-        onClick={(e) => {
-          setInputValue(e.target.value);
-        }}
-      >
-        <option value='BR'>Brasil</option>
-        <option value='PT'>Portugal</option>
-        <option value='ES'>Spain</option>
-        <option value='US'>United States</option>
-        <option value='RU'>Russia</option>
-      </select>
-
+      <a href='/' className='header__title'>
+        City Bike
+      </a>
+      <div>
+        <p>Select country: </p>
+        <select
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        >
+          {selectValues === [] ? (
+            <option value='BR'>Brasil</option>
+          ) : (
+            selectValues.map((value, i) => {
+              return (
+                <option value={value} key={i}>
+                  {value}
+                </option>
+              );
+            })
+          )}
+        </select>
+      </div>
     </header>
   );
 };
